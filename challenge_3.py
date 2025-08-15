2f8c31ed7e75d4a5f469c9a24c82695ff2f44e29
nishikasharon <nishikasharon@gmail.com>
2025-08-15 10:31:04 +0530

    completed a few challenges in hackerrank
    completed 5 challenges regarding python

---
+++ challenge_3.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    total = a+b
+    sub = a-b
+    product = a*b
+    
+    print(total)
+    print(sub)
+    print(product)
+    
---
+++ challenge_5.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        if i < n:
+            pro = i**2
+            print(pro)
+            
