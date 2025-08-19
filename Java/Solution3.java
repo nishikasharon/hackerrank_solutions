4a556aeba625e20cd157a157330b43ef28b7f8e2
nishikasharon <nishikasharon@gmail.com>
2025-08-19 11:32:19 +0530

    Java Hackerrank questions
    Completed 4 questions

---
+++ Java/Solution3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+public class Solution3 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (n%2==0 && 2<=n && n<=5){
+            System.out.println("Not Weird");
+        }
+        else if (n%2==0 && 6<=n && n<=20){
+            System.out.println("Weird");
+        }
+        else if (n%2==0 && 20<n){
+            System.out.println("Not Weird");
+        }
+        else if(n%2!= 0){
+            System.out.println("Weird");
+        }
+            
+        
+        scanner.close();
+    }
+}
+    
+
---
+++ Java/Solution4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Solution4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        
+        String s = scanner.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+        scanner.close();
+        }
+} 
+    
+
